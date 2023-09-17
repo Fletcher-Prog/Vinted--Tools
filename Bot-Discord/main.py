00fba3c30023d6cd375afee76926af036f3a0f6d
the-pokman <66676406+the-pokman@users.noreply.github.com>
2023-09-17 15:46:16 +0200

    First Upload

---
+++ Bot-Discord/main.py
@@ -0,0 +1,9 @@
+# Permet de télecharer une image a partir d'un lien
+#response = requests.get(<lien de l'image>)
+#name_img = 'image.jpg'
+#with open(name_img, "wb") as f:
+#    f.write(response.content)
+
+APPLICATION ID : 1151890848385085523
+
+PUBLIC KEY : b5fc16d713f1ae8ae79f3c913ff32d5c6939171f5fd45183364d3bfbe4adaf44
