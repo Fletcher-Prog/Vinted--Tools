becee52977440ec27f3c1606f80033a516774762
Fletcher-Prog <121747499+Fletcher-Prog@users.noreply.github.com>
2023-11-30 18:41:35 +0100

    Implémentation du chagement de panel inteface graphique Api

---
+++ Interface Graphique Java/PanelRequests.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
