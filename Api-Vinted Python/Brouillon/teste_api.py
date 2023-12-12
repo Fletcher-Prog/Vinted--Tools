baa708fd06ca8fbe0926423d68a32657894c234e
Fletcher-Prog <121747499+Fletcher-Prog@users.noreply.github.com>
2023-12-12 15:12:26 +0100

    Interface Api vinted fonctionnel parti pannel
    maque la lecture de ficher

--- Api-Vinted Python/Brouillon/teste_api.py
+++ Api-Vinted Python/Brouillon/teste_api.py
@@ -1,10 +1,23 @@
-from flask import Flask
+import requests
+from bs4 import BeautifulSoup
 
-# For start project flask --app name_file_project run
 
 
-app = Flask(__name__)
+url ="https://www.vinted.fr/catalog?search_text=sweat%20lacoste&price_to=15&currency=EUR&size_ids[]=207&size_ids[]=208&status_ids[]=1&status_ids[]=2&order=newest_first"
 
-@app.route('/{name}')
-def vinted_link():
-    return name
+agents = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0'}
+
+html_content = requests.get(url, headers = agents ).text
+
+print(html_content)
+
+#if html_content.status_code == 200 :
+	
+pageRepondu = BeautifulSoup(html_content, 'html.parser')
+
+
+pageRepondu.find('//div[@class="feed-grid__item-content"]') 
+
+# Écrire le contenu HTML dans un fichier
+with open("page.html", "w", encoding="utf-8") as file:
+    file.write(str(pageRepondu.find('div[@class="feed-grid__item-content"]')) )
--- Interface Graphique Java/Ihm.java
+++ Interface Graphique Java/Ihm.java
@@ -1,16 +1,35 @@
 import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import java.awt.*;
 
 
 public class Ihm extends JFrame
 {
-	// Panelle overlay droite
-	private JPanel panelOverlayDroite = new JPanel(new FlowLayout()) ;
-	private JPanel panelOverlayHaut   = new JPanel(new FlowLayout(FlowLayout.CENTER)) ;
-	private JButton buttonPassageLog        = new JButton("Log");
+	// Panel overlay droite
+	private JPanel     panelOverlayDroite      = new JPanel(new FlowLayout()) ;
+	private JPanel     panelOverlayHaut        = new JPanel(new FlowLayout(FlowLayout.CENTER)) ;
+	
+	// Pannel log et requette
+	private JPanel     pannelLog               = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+	private JPanel     pannelRequette          = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+	
+	// Boutton de pasage entre les pannels
+	private JButton    buttonPassageLog        = new JButton("Log");
+	private JButton    buttonPassageRequette   = new JButton("Requette");
+	
+	// Couleur personaliser
+	private Color      couleurPannelLog        =  new Color(0xFFA500); 
+	private Color      couleurPannelReqette    =  new Color(0x0000FF);
+	private Color      couleurPannelOverlay    =  new Color(0x252525);
+	
+	// Permet le switch entre les pannel sans les re construire
+	private CardLayout listePannel             = new CardLayout();
+	private JPanel     listeDesPannel          = new JPanel(listePannel);
 
-
-
+	// Constructucteur
     public Ihm()
     {
         super("Api vinted");
@@ -26,38 +45,85 @@ public class Ihm extends JFrame
 		// Rendre la fentre non redimentionnable
 		this.setResizable(false);
 		
+		// Changement de pannel en fonction du boutton appuiyer
+		buttonPassageLog.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				listePannel.show(listeDesPannel,"pannelRequette");
+
+			}
+		});
+
+		buttonPassageRequette.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				listePannel.show(listeDesPannel,"pannelLog");
+			}
+		});
+
 		// Construction de l'overlay 
 		overlay();
+		
+		// Construction du pannel requette
+		pannelDesRequette();
+		
+		// Construction du pannel des logs
+		pannelDesLog();
 
+		// Ajou des pannels grace a la liste
+		this.add(listeDesPannel, BorderLayout.CENTER);
+		
 		// Rendre la fenetre soit visible
         this.setVisible(true);
 
     }
 
+	// Construction de l'overlay
 	public  void overlay()
 	{
-		// Contruction est ajout de l'overlay
+		// Contruction des ajout pour l'overlay
 		panelOverlayDroite.setPreferredSize(new Dimension(55 , 50 ));
-		panelOverlayDroite.setBackground(Color.BLACK) ;
+		panelOverlayDroite.setBackground(couleurPannelOverlay) ;
 
-		panelOverlayHaut.add(buttonPassageLog , BorderLayout.CENTER);
 		panelOverlayHaut.setPreferredSize(new Dimension(50 , 50 ));
-		panelOverlayHaut.setBackground(Color.BLACK) ;
+		panelOverlayHaut.setBackground(couleurPannelOverlay) ;
 		
-		
-		// Ajout de l'overlay
+		// Ajout des element au pannel 
 		this.add(panelOverlayDroite, BorderLayout.WEST );
 		this.add(panelOverlayHaut, BorderLayout.NORTH );
+		panelOverlayHaut.add(buttonPassageRequette , BorderLayout.CENTER);
+		panelOverlayHaut.add(buttonPassageLog , BorderLayout.CENTER);		
+	}
 
+	// Pannel requette
+	public void pannelDesRequette()
+	{
+		pannelRequette.setPreferredSize(new Dimension(450,400));
 
-		
+		pannelRequette.setBackground(couleurPannelReqette);
+
+		listeDesPannel.add(pannelRequette,"pannelRequette") ;
+	}
+
+	// Pannel de log
+	public void pannelDesLog()
+	{
+		pannelLog.setPreferredSize(new Dimension(450,400));
+
+		pannelLog.setBackground(couleurPannelLog);
+
+		//this.add(pannelLog, BorderLayout.EAST) ;		
+		listeDesPannel.add(pannelLog,"pannelLog") ;
+
 	}
 
     public static void main(String[] args)
     {
         new Ihm();
     }
-
-
 
 }
