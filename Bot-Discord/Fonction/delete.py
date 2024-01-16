15a5e881578fa0c4e95e0faa65c4b92c5d9d4b17
Fletcher-Prog <121747499+Fletcher-Prog@users.noreply.github.com>
2024-01-16 17:05:05 +0100

    Fix Delete commande

--- Bot-Discord/Fonction/delete.py
+++ Bot-Discord/Fonction/delete.py
@@ -1,23 +1,26 @@
-import re
-
 def delete(file_path,elementASupprimer):
 
-    # Lire le fichier
-    with open(file_path, 'r') as file:
-        content = file.read()
+    finalContent = ""
 
-    
-    if elementASupprimer in content:
-        # Remplacer les URL par un espace, puis supprimer les espaces supplémentaires
-        content_without_urls = re.sub(elementASupprimer, ' ', content).strip()
-        modified_content = re.sub(r'\s+', '\n', content_without_urls)
+    with open(file_path,'r') as file : 
+        content = file.readlines()
 
-        # Écrire le contenu modifié dans le fichier
-        with open(file_path, 'w') as file:
-            file.write(modified_content)
-            file.close()
+    #print(content)
+
+    if elementASupprimer in content :
+
+        for ligne in content :        
+
+            if ligne.strip() != elementASupprimer.strip() :
+                
+                finalContent = finalContent + ligne
+
+        with open(file_path,'w') as file : 
+            file.write(finalContent)
+            file.close
         
         return True
-    
+
     else :
-        return False
+
+       return False
